74311323fe3a78122cf73aeb890ec4da35d8e24b
yutakaki <yu.takaki2000@gmail.com>
2021-11-10 14:52:56 +0900

    feat: added folders

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
 				<p>
-					Edit <code>src/Apjs</code> and save to reload.
+					Edit <code>src/Apjs</code> and save to rd.
 				</p>
 				<a
 					className="App-link"
