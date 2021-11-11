2dbefc478b45ab57668f0b35a4e9267dd566e5fb
yutakaki <yu.takaki2000@gmail.com>
2021-11-11 17:34:56 +0900

    feat:fix profile

--- src/components/owners/OwnerHome.jsx
+++ src/components/owners/OwnerHome.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import "../../styles/owners/ownerHome.scss";
+import sampleImg from "../../assets/sample.jpg";
+import sampleDog from "../../assets/sampleDog2.jpeg";
 
 const OwnerHome = () => {
 	return (
-		<main>
-			<h1>Owner Page</h1>
+		<main className="owner">
+			<div className="profileContainer">
+				<div className="profile">
+					<img src={sampleImg} alt="profile"></img>
+				</div>
+			</div>
+			<section className="homeSection">
+				<figure>
+					<img src={sampleDog} alt="dog"></img>
+				</figure>
+				<div className="calendar"></div>
+			</section>
 		</main>
 	);
 };
