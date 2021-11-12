49563b92bc92a268aac834468e728e6d2520d254
yutakaki <yu.takaki2000@gmail.com>
2021-11-12 21:29:16 +0900

    feat: routes finish beta 1

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,6 +5,7 @@ import { useSelector } from "react-redux";
 
 const Navbar = () => {
 	const type = useSelector((state) => state.user.type);
+	const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
 	return (
 		<header className="navHeader">
 			<nav className="navMain">
@@ -21,14 +22,33 @@ const Navbar = () => {
 							</Link>
 						</>
 					)}
-					<Link to="/messenger">
-						<li>
-							<i className="fa fa-wechat"></i>
-						</li>
-					</Link>
-					<li>
-						<i className="fa fa-sign-out"></i>
-					</li>
+					{isLoggedIn === true && (
+						<>
+							<Link to="/messenger">
+								<li>
+									<i className="fa fa-wechat"></i>
+								</li>
+							</Link>
+							<li>
+								<i className="fa fa-sign-out"></i>
+							</li>
+						</>
+					)}
+
+					{isLoggedIn === false && (
+						<>
+							<Link to="/signin">
+								<li>
+									<i>log in</i>
+								</li>
+							</Link>
+							<Link to="/signup">
+								<li>
+									<i>sign up</i>
+								</li>
+							</Link>
+						</>
+					)}
 				</ul>
 			</nav>
 		</header>
