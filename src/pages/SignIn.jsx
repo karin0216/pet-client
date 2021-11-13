baed72371bea2cc037dd049762c7b51da5fd7756
Eiko0705 <eiko.imai.070502@gmail.com>
2021-11-13 20:49:05 +0900

    feat: create the validation for sign up page

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,20 +1,37 @@
-import React, { createRef } from "react";
+import React from "react";
 import { useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { signIn } from "../slicers/userSlice";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yup from "yup";
 
 export default function SignIn() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const email = createRef();
-  const password = createRef();
 
-  const handleSignIn = async (e) => {
-    e.preventDefault();
+  // this code is for validation
+  // validation rule
+  const validationSchema = Yup.object().shape({
+    email: Yup.string().required("Emai is required").email("Email is invalid"),
+    password: Yup.string().required("Password is required"),
+  });
+
+  // pass the validation rule to the useForm func
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(validationSchema),
+  });
+
+  // when the form is valid and submitted, this method is called
+  const handleSignIn = async (data) => {
     const signInAction = await dispatch(
       signIn({
-        email: email.current.value,
-        password: password.current.value,
+        email: data.email,
+        password: data.password,
       })
     );
     if (signInAction.payload.user) {
@@ -25,10 +42,22 @@ export default function SignIn() {
   return (
     <>
       <div className="sign-in-container" style={{ marginTop: 200 }}>
-        <form onSubmit={handleSignIn}>
+        <form onSubmit={handleSubmit(handleSignIn)}>
           <h1>Sign In</h1>
-          <input type="text" placeholder="Email" ref={email} />
-          <input type="password" placeholder="Password" ref={password} />
+          <input
+            name="email"
+            type="text"
+            placeholder="Email"
+            {...register("email")}
+          />
+          <div>{errors.email?.message}</div>
+          <input
+            name="password"
+            type="password"
+            placeholder="Password"
+            {...register("password")}
+          />
+          <div>{errors.password?.message}</div>
           <button>Sign In</button>
         </form>
       </div>
