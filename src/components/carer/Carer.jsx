d451f55957111c1b7715779dbba2ceecfec064c2
karin0216 <cheer.karin0216@gmail.com>
2021-11-14 19:30:57 +0900

    working on searching

--- src/components/carer/Carer.jsx
+++ src/components/carer/Carer.jsx
@@ -6,43 +6,61 @@ const { REACT_APP_SERVER_URL } = process.env;
 
 // Grid of cards for web view, column for phone view
 
-//TODO: if type is not selected, append all pets 
+//TODO: if type is not selected, append all pets
 const Carer = () => {
-	const [type, setType] = useState("");
-	const [pets, setPets] = useState([]);
-	const types = ["Dog", "Cat", "Otter", "Snake"];
-	const typeRef = useRef();
+  const [all, setAll] = useState([]);
+  const [view, setView] = useState("All");
+  const [type, setType] = useState("");
+  const [pets, setPets] = useState([]);
+  const types = ["Dog", "Cat", "Otter", "Snake"];
+  const typeRef = useRef();
 
-	useEffect(() => {
-		if (type) {
-			const fetchPets = async () => {
-				const pets = await axios.get(`${REACT_APP_SERVER_URL}/pet/type/${type}`);
-				setPets(pets.data);
-			}
-			fetchPets();
-		}
-	}, [type]);
+  useEffect(() => {
+    const fetchAllPets = async () => {
+      try {
+        const pets = await axios.get(`${REACT_APP_SERVER_URL}/pet`);
+        console.log(pets.data);
+        setAll(pets.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchAllPets();
+  }, [all]);
 
-	return (
-		<main className="carerMain">
-			<section>
-				<select
-					name="type"
-					defaultValue=""
-					ref={typeRef}
-					onChange={(e) => setType(e.target.value)}
-				>
-					<option value="">Select pet type</option>
-					{types.map((type, i) => (
-						<option key={i} value={type}>
-							{type}
-						</option>
-					))}
-				</select>
-			</section>
-			<PetGrid pets={pets}/>
-		</main>
-	);
+  useEffect(() => {
+    if (type) {
+      const fetchPets = async () => {
+        const pets = await axios.get(
+          `${REACT_APP_SERVER_URL}/pet/type/${type}`
+        );
+        setPets(pets.data);
+        setView("Type");
+      };
+      fetchPets();
+    }
+  }, [type]);
+
+  return (
+    <main className="carerMain">
+      <section>
+        <select
+          name="type"
+          defaultValue=""
+          ref={typeRef}
+          onChange={(e) => setType(e.target.value)}
+        >
+          <option value="">Select pet type</option>
+          {types.map((type, i) => (
+            <option key={i} value={type}>
+              {type}
+            </option>
+          ))}
+        </select>
+      </section>
+      <PetGrid pets={pets} />
+    </main>
+  );
 };
 
 export default Carer;
--- src/pages/Pet.jsx
+++ src/pages/Pet.jsx
@@ -11,46 +11,51 @@ const { REACT_APP_SERVER_URL } = process.env;
 // Has a date picker for choosing the dates for requested
 // Has the button to direct to the questionnaire page
 const Pet = () => {
-	const [petInfo, setPetInfo] = useState({});
-	const { id } = useParams();
-	useEffect(() => {
-		(async () => {
-			try {
-				const pet = await axios.get(`${REACT_APP_SERVER_URL}/pet/${id}`)
-				setPetInfo(pet.data)
-			} catch (error) {
-				console.log(error);
-			}
-		})();
-	}, [id]);
+  const [petInfo, setPetInfo] = useState({});
+  const { id } = useParams();
+  useEffect(() => {
+    (async () => {
+      try {
+        const pet = await axios.get(`${REACT_APP_SERVER_URL}/pet/${id}`);
+        setPetInfo(pet.data);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, [id]);
 
-	return (
-		<main className="petMain">
-			<div className="container-fluid p-3 align-items-center">
-				<section className="petOptions">
-					<DatePicker />
-					<Link to="/carer/questionnaire">
-						<button className="card-button">Book Date</button>
-					</Link>
-				</section>
-				<section className="petFlexBox">
-					<figure>
-						<div className="mainPic">
-							{petInfo.pet_pictures ?
-								<img src={`${REACT_APP_SERVER_URL}/pic/${petInfo.pet_pictures}`} alt="pet pic" />
-								: <img src={samplePet} alt="pet" />}
-						</div>
-					</figure>
+  return (
+    <main className="petMain">
+      <div className="container-fluid p-3 align-items-center">
+        <section className="petOptions">
+          <DatePicker />
+          <Link to="/carer/questionnaire">
+            <button className="card-button">Book Date</button>
+          </Link>
+        </section>
+        <section className="petFlexBox">
+          <figure>
+            <div className="mainPic">
+              {petInfo.pet_pictures ? (
+                <img
+                  src={`${REACT_APP_SERVER_URL}/pic/${petInfo.pet_pictures}`}
+                  alt="pet pic"
+                />
+              ) : (
+                <img src={samplePet} alt="pet" />
+              )}
+            </div>
+          </figure>
 
-					<div className="petBio">
-						<h1>This is {petInfo.name}</h1>
-						<h3>Bio</h3>
-						<p>{petInfo.description}</p>
-					</div>
-				</section>
-			</div>
-		</main>
-	);
+          <div className="petBio">
+            <h1>This is {petInfo.name}</h1>
+            <h3>Bio</h3>
+            <p>{petInfo.description}</p>
+          </div>
+        </section>
+      </div>
+    </main>
+  );
 };
 
 export default Pet;
