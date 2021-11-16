6508d03b57b846c8f1698abc11bb71161342e3a4
yutakaki <yu.takaki2000@gmail.com>
2021-11-16 23:46:22 +0900

    merge

--- src/components/owners/AnswerForm.jsx
+++ src/components/owners/AnswerForm.jsx
@@ -3,69 +3,69 @@ import axios from "axios";
 const { REACT_APP_SERVER_URL } = process.env;
 
 const AnswerForm = ({
-	answerFormRef,
-	setCurrentRequest,
-	currentRequest,
-	getRequests,
+  answerFormRef,
+  setCurrentRequest,
+  currentRequest,
+  getRequests,
 }) => {
-	console.log(currentRequest);
-	const closeAnswerForm = () => {
-		setCurrentRequest({});
-		answerFormRef.current.classList.remove("showAnswerForm");
-	};
-	const requestAction = async (action) => {
-		try {
-			await axios.patch(
-				`${REACT_APP_SERVER_URL}/requests/${action}`,
-				{
-					user_id: currentRequest._id,
-					request_id: currentRequest.request._id,
-					action,
-				},
-				{
-					headers: {
-						"x-access-token": localStorage.getItem("token"),
-					},
-				}
-			);
-			if (action === "accept") {
-				await axios.post(
-					`${REACT_APP_SERVER_URL}/messages/conversations`,
-					{ user_id: currentRequest._id },
-					{
-						headers: {
-							"x-access-token": localStorage.getItem("token"),
-						},
-					}
-				);
-			}
-			await getRequests();
-			closeAnswerForm();
-		} catch (error) {
-			console.log(error);
-		}
-	};
-	return (
-		<div className="answerFormContainer" ref={answerFormRef}>
-			<div className="form">
-				<i className="fa fa-close" onClick={closeAnswerForm}></i>
-				<h1>Answers</h1>
-				<ol>
-					{currentRequest.request &&
-						currentRequest.request.questionnaire.map((qa, i) => (
-							<li>
-								<h2>{qa.question}</h2>
-								<p>{qa.answer}</p>
-							</li>
-						))}
-				</ol>
-				<div className="answerFormButtons">
-					<button onClick={() => requestAction("accept")}>Accept</button>
-					<button onClick={() => requestAction("reject")}>Decline</button>
-				</div>
-			</div>
-		</div>
-	);
+  console.log(currentRequest);
+  const closeAnswerForm = () => {
+    setCurrentRequest({});
+    answerFormRef.current.classList.remove("showAnswerForm");
+  };
+  const requestAction = async (action) => {
+    try {
+      await axios.patch(
+        `${REACT_APP_SERVER_URL}/requests/${action}`,
+        {
+          user_id: currentRequest._id,
+          request_id: currentRequest.request._id,
+          action,
+        },
+        {
+          headers: {
+            "x-access-token": localStorage.getItem("token"),
+          },
+        }
+      );
+      if (action === "Approved") {
+        await axios.post(
+          `${REACT_APP_SERVER_URL}/messages/conversations`,
+          { user_id: currentRequest._id },
+          {
+            headers: {
+              "x-access-token": localStorage.getItem("token"),
+            },
+          }
+        );
+      }
+      await getRequests();
+      closeAnswerForm();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div className="answerFormContainer" ref={answerFormRef}>
+      <div className="form">
+        <i className="fa fa-close" onClick={closeAnswerForm}></i>
+        <h1>Answers</h1>
+        <ol>
+          {currentRequest.request &&
+            currentRequest.request.questionnaire.map((qa, i) => (
+              <li>
+                <h2>{qa.question}</h2>
+                <p>{qa.answer}</p>
+              </li>
+            ))}
+        </ol>
+        <div className="answerFormButtons">
+          <button onClick={() => requestAction("Approved")}>Accept</button>
+          <button onClick={() => requestAction("reject")}>Decline</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AnswerForm;
--- src/components/owners/PetSchedule.jsx
+++ src/components/owners/PetSchedule.jsx
@@ -1,58 +1,81 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import RequestCell from "./RequestCell";
 import Calendar from "react-calendar";
+import axios from "axios";
+const { REACT_APP_SERVER_URL } = process.env;
 
 const PetSchedule = () => {
-	const [scheduleState, setScheduleState] = useState("calendar");
-	const mark = [
-		{
-			start: "11-11-2021",
-			end: "12-16-2021",
-		},
-	];
-	const onActiveStartDateChange = (e) => {
-		console.log(e);
-	};
-	return (
-		<section className="calendar">
-			<div className="scheduleOption">
-				<i
-					className={`fa fa-calendar ${
-						scheduleState === "calendar" && "activePetOptionSchedule"
-					}`}
-					onClick={() => setScheduleState("calendar")}></i>
-				<i
-					className={`fa fa-list-ul ${
-						scheduleState !== "calendar" && "activePetOptionSchedule"
-					}`}
-					onClick={() => setScheduleState("list")}></i>
-				<p>Pet schedule</p>
-			</div>
+  const [scheduleState, setScheduleState] = useState("calendar");
+  const [schedule, setSchedule] = useState([]);
+  const onActiveStartDateChange = (e) => {
+    console.log(e);
+  };
 
-			{scheduleState === "calendar" ? (
-				<Calendar
-					onChange={onActiveStartDateChange}
-					tileClassName={({ date, view }) => {
-						if (
-							mark.find(
-								(x) =>
-									new Date(x.start) <= new Date(date) &&
-									new Date(x.end) >= new Date(date)
-							)
-						) {
-							return "highlight";
-						}
-					}}
-				/>
-			) : (
-				<ul>
-					{[1, 2, 3, 4, 5, 6, 7, 8, 9].map(() => (
-						<RequestCell />
-					))}
-				</ul>
-			)}
-		</section>
-	);
+  useEffect(() => {
+    (async () => {
+      try {
+        const request = await axios.get(
+          `${REACT_APP_SERVER_URL}/requests/Approved`,
+          {
+            headers: {
+              "x-access-token": localStorage.getItem("token"),
+            },
+          }
+        );
+        setSchedule(
+          request.data.map((req) => {
+            return {
+              ...req.request,
+              fullname: req.username,
+              profile_picture: req.profile_picture,
+            };
+          })
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
+  return (
+    <section className="calendar">
+      <div className="scheduleOption">
+        <i
+          className={`fa fa-calendar ${
+            scheduleState === "calendar" && "activePetOptionSchedule"
+          }`}
+          onClick={() => setScheduleState("calendar")}></i>
+        <i
+          className={`fa fa-list-ul ${
+            scheduleState !== "calendar" && "activePetOptionSchedule"
+          }`}
+          onClick={() => setScheduleState("list")}></i>
+        <p>Pet schedule</p>
+      </div>
+
+      {scheduleState === "calendar" ? (
+        <Calendar
+          onChange={onActiveStartDateChange}
+          tileClassName={({ date, view }) => {
+            if (
+              schedule.find(
+                (x) =>
+                  new Date(x.start) <= new Date(date) &&
+                  new Date(x.end) >= new Date(date)
+              )
+            ) {
+              return "highlight";
+            }
+          }}
+        />
+      ) : (
+        <ul>
+          {schedule.map((sched) => (
+            <RequestCell sched={sched} />
+          ))}
+        </ul>
+      )}
+    </section>
+  );
 };
 
 export default PetSchedule;
