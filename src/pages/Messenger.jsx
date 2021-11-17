3a38199fbaed0b6daafd8f891e0705696e717539
yutakaki <yu.takaki2000@gmail.com>
2021-11-17 18:20:49 +0900

    init commit

--- src/pages/Messenger.jsx
+++ src/pages/Messenger.jsx
@@ -1,39 +1,48 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import Contacts from "../components/messenger/Contacts";
 import MessageBox from "../components/messenger/MessageBox";
+import { signOutMessengerCleanUp } from "../slicers/messengerSlice";
 import "../styles/messenger/messenger.scss";
 
 const Messenger = () => {
-	const closeMessengerBox = () => {
-		document
-			.querySelector(".messageBoxContainer")
-			.classList.add("hideMessenger");
-		document
-			.querySelector(".contactsListContainer")
-			.classList.remove("hideMessenger");
+  const dispatch = useDispatch();
+  const closeMessengerBox = () => {
+    document
+      .querySelector(".messageBoxContainer")
+      .classList.add("hideMessenger");
+    document
+      .querySelector(".contactsListContainer")
+      .classList.remove("hideMessenger");
 
-		document.querySelector(".messageBoxHeader").classList.add("hideHeader");
-		document.querySelector(".contactListHeader").classList.remove("hideHeader");
-	};
-	return (
-		<main className="messengerPage">
-			<div className="messengerContainer">
-				<div className="messengerHeader">
-					<div className="contactListHeader">
-						<h2>Contact</h2>
-					</div>
-					<div className="messageBoxHeader hideHeader">
-						<i className="fa fa-angle-left" onClick={closeMessengerBox}></i>
-						<h2>Message</h2>
-					</div>
-				</div>
-				<section className="messengerContent">
-					<Contacts />
-					<MessageBox />
-				</section>
-			</div>
-		</main>
-	);
+    document.querySelector(".messageBoxHeader").classList.add("hideHeader");
+    document.querySelector(".contactListHeader").classList.remove("hideHeader");
+  };
+
+  useEffect(() => {
+    return () => {
+      dispatch(signOutMessengerCleanUp());
+    };
+  }, [dispatch]);
+  return (
+    <main className="messengerPage">
+      <div className="messengerContainer">
+        <div className="messengerHeader">
+          <div className="contactListHeader">
+            <h2>Contact</h2>
+          </div>
+          <div className="messageBoxHeader hideHeader">
+            <i className="fa fa-angle-left" onClick={closeMessengerBox}></i>
+            <h2>Message</h2>
+          </div>
+        </div>
+        <section className="messengerContent">
+          <Contacts />
+          <MessageBox />
+        </section>
+      </div>
+    </main>
+  );
 };
 
 export default Messenger;
