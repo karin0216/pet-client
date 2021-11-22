d0d426151af89d479121d9ef2f6021aec332ba37
karin0216 <cheer.karin0216@gmail.com>
2021-11-22 14:55:41 +0900

    fixed conflict

--- src/components/owners/OwnerHome.jsx
+++ src/components/owners/OwnerHome.jsx
@@ -1,15 +1,9 @@
 import React from "react";
-
 import PetInfo from "./PetInfo";
 import { Routes, Route } from "react-router-dom";
 import Request from "./Request";
-<<<<<<< HEAD
-import { useSelector } from "react-redux";
 import UpdatePetInfo from "./UpdatePetInfo";
-const { REACT_APP_SERVER_URL } = process.env;
-=======
 import UserProfile from "../UserProfile";
->>>>>>> main
 
 const OwnerHome = () => {
   return (
