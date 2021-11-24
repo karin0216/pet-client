f93d5898d6f9653f9931403f0108d8747ed180c6
yutakaki <yu.takaki2000@gmail.com>
2021-11-24 20:09:23 +0900

    feat/add state

--- src/components/carer/PetGrid.jsx
+++ src/components/carer/PetGrid.jsx
@@ -6,9 +6,11 @@ const PetGrid = () => {
   const allPets = useSelector((state) => state.filterOptions.allPets);
   const filteredPets = useSelector((state) => state.filterOptions.filteredPets);
   const isFiltered = useSelector((state) => state.filterOptions.isFiltered);
+  const filterState = useSelector((state) => state.filterOptions.state);
   return (
     <div className="container">
       <div className="row">
+        <h1>{filterState}</h1>
         {!isFiltered ? (
           allPets.map((pet, i) => <PetCard pet={pet} key={i} />)
         ) : isFiltered && filteredPets.length ? (
