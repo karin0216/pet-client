cb50398452c6da45066ded8974fa65b8ff58fd27
karin0216 <cheer.karin0216@gmail.com>
2021-11-26 16:23:27 +0900

    replace upload image function to util's one

--- src/pages/UpdateUserInfoCarer.jsx
+++ src/pages/UpdateUserInfoCarer.jsx
@@ -6,9 +6,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as Yup from "yup";
-import axios from "axios";
-const { REACT_APP_SERVER_URL } = process.env;
-// import { submitPic } from "../util/uploadImage";
+import { submitPic } from "../util/uploadImage";
 
 const UpdateUserInfo = () => {
   const [errorMessage, setErrorMessage] = useState("");
@@ -88,21 +86,6 @@ const UpdateUserInfo = () => {
   };
 
   const onSubmit = async (data) => {
-    const submitPic = async (imageInput) => {
-      try {
-        const formData = new FormData();
-        formData.append("file", imageInput);
-
-        const response = await axios.post(
-          `${REACT_APP_SERVER_URL}/pic/upload`,
-          formData
-        );
-        return response.data[0].filename;
-      } catch (err) {
-        console.log(err);
-      }
-    };
-
     let img;
     if (data.profile_picture[0] !== undefined) {
       img = await submitPic(data.profile_picture[0]);
@@ -216,7 +199,8 @@ const UpdateUserInfo = () => {
               type="type"
               className="input"
               placeholder="Bio"
-              {...register("description")}></textarea>
+              {...register("description")}
+            ></textarea>
           </div>
           <h3>Interest</h3>
           <h5>Pet Type:</h5>
